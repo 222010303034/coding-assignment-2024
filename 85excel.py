10f71de1de2a0e40e1a3f4c01f29bd5a6796e3ea
222010303034 <83938894+222010303034@users.noreply.github.com>
2024-01-21 11:36:30 +0530

    Update 85excel.py

--- 85excel.py
+++ 85excel.py
@@ -1,18 +1,9 @@
-# Python program to return title to result 
-# of excel sheet. 
-
-# Returns result when we pass title. 
-def titleToNumber(s):
-	# This process is similar to binary-to- 
-	# decimal conversion 
+ 
+def titleToNumber(s): 
 	result = 0; 
 	for B in range(len(s)): 
 		result *= 26; 
 		result += ord(s[B]) - ord('A') + 1; 
 
 	return result; 
-
-# Driver function 
 print(titleToNumber("CDA")); 
-
-# This code contributed by Rajput-Ji
